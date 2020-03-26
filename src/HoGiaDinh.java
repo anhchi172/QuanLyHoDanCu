f9c7cd420acb8767fe47fa896e384751606afd96
anhchi172 <anhchi172@gmail.com>
2020-03-26 15:28:44 -0400

    Add Class DAO

--- src/HoGiaDinh.java
+++ src/HoGiaDinh.java
@@ -1,6 +1,7 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class HoGiaDinh {
+public class HoGiaDinh implements Serializable {
     private ArrayList<Nguoi> al;
     public HoGiaDinh() {
         al = new ArrayList<Nguoi>();
--- src/KhuPho.java
+++ src/KhuPho.java
@@ -2,14 +2,20 @@ import java.util.ArrayList;
 
 public class KhuPho {
     private ArrayList<HoGiaDinh> al;
+    private HoDanCuDAO hoDanCuDAO;
 
     public KhuPho() {
-        al = new ArrayList<HoGiaDinh>();
+
+       hoDanCuDAO = new HoDanCuDAO();
+       al = (ArrayList<HoGiaDinh>) hoDanCuDAO.read();
+
     }
 
     public void addHoGiaDinh(HoGiaDinh h) {
         if (h != null) {
             al.add(h);
+            hoDanCuDAO.write(al);
+
         }
         else{
                 System.out.println("Loi: ho gia dinh = null");
